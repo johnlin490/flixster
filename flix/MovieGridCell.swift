56eacf78bd3acaac547d60a6d45e62d183fa5671
John Lin <john.lin490@gmail.com>
2020-10-02 20:00:35 -0400

    Users can now tap cells to see more information about the movie and they can see a collection of superhero movie posters by tapping the tap bar button.

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by John Lin on 9/29/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
